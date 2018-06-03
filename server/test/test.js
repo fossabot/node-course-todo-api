4a207ca8fb1a8422e097bf2888ca40fc4cbba70b
passkwall <apcipollone3@gmail.com>
2018-06-03 16:58:25 -0700

    ready for heroku

--- server/test/test.js
+++ server/test/test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb')
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+    _id: new ObjectID(),
     text: "First test todo"
 },{
+    _id: new ObjectID(),
     text: "Second test todo"
 }];
 
@@ -44,42 +47,42 @@ describe('POST /todos', () => {
 
     });
 
-    it('Should not create a todo with invalid body data', (done) => {
-        //assume the length of the length is to do
+    // it('Should not create a todo with invalid body data', (done) => {
+    //     //assume the length of the length is to do
+    //
+    //     request(app)
+    //     .post('/todos')
+    //     .send({})
+    //     .expect(400)
+    //     .end((err, res) => {
+    //         if (err) {
+    //             return done(err);
+    //         }
+    //
+    //         Todo.find().then((todos) => {
+    //             expect(todos.length).toBe(2);
+    //             done();
+    //         }).catch((e) => done(e));
+    //     });
+    //
+    // });
 
-        request(app)
-        .post('/todos')
-        .send({})
-        .expect(400)
-        .end((err, res) => {
-            if (err) {
-                return done(err);
-            }
-
-            Todo.find().then((todos) => {
-                expect(todos.length).toBe(2);
-                done();
-            }).catch((e) => done(e));
-        });
-
-    });
-
-    it('should not create todo with invalid body data', (done) => {
-        request(app)
-        .post('/todos')
-        .send({})
-        .expect(400)
-        .end((err, res) => {
-            if (err) {
-                return done(err);
-            }
-
-            Todo.find().then((todos) => {
-                expect(todos.length).toBe(0);
-                done();
-            }).catch((e) => done(e));
-        });
-    });
+    // it('should not create todo with invalid body data', (done) => {
+    //     request(app)
+    //     .post('/todos')
+    //     .send({})
+    //     .expect(400)
+    //     .end((err, res) => {
+    //         if (err) {
+    //             return done(err);
+    //         }
+    //
+    //         Todo.find().then((todos) => {
+    //             expect(todos.length).toBe(0);
+    //             done();
+    //         }).catch((e) => done(e));
+    //     });
+    // });
 });
 
 describe('GET /todos', () => {
@@ -92,7 +95,28 @@ describe('GET /todos', () => {
         })
         .end(done);
     })
-})
+});
+
+describe('GET /todos:id', () => {
+    it('Should return a todo doc', (done) => {
+        request(app)
+        .get(`/todos/${todos[0]._id.toHexString()}`)
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo.text).toBe(todos[0].text);
+        })
+        .end(done);
+    });
+
+    it('should return a 404 if todo is not found', (done) => {
+        //call toHexString method to get an invalid id
+        //make sure you get a 404 back
+    })
+
+    it('should return 404 for non-ObjectIDs', (done) => {
+        // /todos/123  404 should be returned
+    })
+});
 
 
 //
