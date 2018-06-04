866d6e76f41e3a1b8c6a35164025ce4212d589dd
passkwall <apcipollone3@gmail.com>
2018-06-03 17:41:03 -0700

    added DELETE method

--- server/server.js
+++ server/server.js
@@ -64,6 +64,31 @@ app.get('/todos/:id', (req,res) => {
     });
 });
 
+app.delete('/todos/:id', (req,res) => {
+    // get the ID from the URL
+    var id = req.params.id;
+    //validate the ID.  if not valid, return a 404
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send('That is not a valid ID');
+    };
+    //remove todo by the id
+    Todo.findByIdAndRemove(id).then((todo) => {
+        //success
+            //if no doc, send 404
+        if (!todo) {
+            return res.status(404).send('No Todo by that ID');
+        };
+            //if doc, send doc back with 200
+        res.status(200).send('Your Todo was removed');
+    }, (e) => {
+        //error
+            //400 with empty body
+        return res.send('Something went wrong..');
+    }).catch((e) => {
+        res.status(500).send();
+    })
+});
+
 
 app.listen(port, () => {
     console.log(`Started up on port ${port}`);
