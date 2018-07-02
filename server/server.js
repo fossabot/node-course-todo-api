b0852b7524aba50f21d9fd2026ff9fc3e8ff64ad
passkwall <apcipollone3@gmail.com>
2018-07-01 21:25:21 -0700

    update config to use JSON

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const {ObjectID} = require('mongodb');
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+const {userDrinkRecord} = require('./models/userDrinkRecord')
 var {authenticate} = require('./middleware/authenticate')
 
 const app = express();
